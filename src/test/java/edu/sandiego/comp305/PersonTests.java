535dcd6cfc1daf81ca43ab72956f26b692f067c4
Penny Yanez <pyanez@sandiego.edu>
2026-04-27 10:28:01 -0700

    feat:Initial test stub for implementing getName() method

--- src/test/java/edu/sandiego/comp305/PersonTests.java
+++ src/test/java/edu/sandiego/comp305/PersonTests.java
@@ -1,4 +1,13 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PersonTests {
+    @Test
+    public void testGetName() {
+        Person person = new Person();
+        assertEquals("Name", person.getName());
+    }
 }
