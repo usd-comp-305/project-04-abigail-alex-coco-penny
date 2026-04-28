9020841ac5193a535ef2872b202c23ae0ad029c8
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 09:34:54 -0700

    Feat: Initial test stub for getBankBalance() to be able to calculate bank

--- src/test/java/edu/sandiego/comp305/PartnerTests.java
+++ src/test/java/edu/sandiego/comp305/PartnerTests.java
@@ -1,4 +1,14 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PartnerTests {
+    @Test
+    public void testGetBankBalance(){
+        Partner partner = new Partner();
+        assertEquals(1000, partner.getBankBalance());
+
+    }
 }
