6721c8c404460c491469240852924f098010fb54
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 09:42:38 -0700

    Feat: initial test stub for setBankBalance to be able to change balance amount as game progresses

--- src/test/java/edu/sandiego/comp305/PartnerTests.java
+++ src/test/java/edu/sandiego/comp305/PartnerTests.java
@@ -9,6 +9,12 @@ public class PartnerTests {
     public void testGetBankBalance(){
         Partner partner = new Partner();
         assertEquals(1000, partner.getBankBalance());
+    }
 
+    @Test
+    public void testGetBankBalance(){
+        Partner partner = new Partner();
+        partner.setBankBalance(1000);
+        assertEquals(1000, partner.getBankBalance());
     }
 }
