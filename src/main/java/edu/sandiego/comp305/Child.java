7e658b07fc13f48f5ef0d970068ee8948daa0844
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 13:40:32 -0700

    Feat: initial method stub extended method from Parent indicating which stage of life class represents

--- src/main/java/edu/sandiego/comp305/Child.java
+++ src/main/java/edu/sandiego/comp305/Child.java
@@ -3,9 +3,14 @@ package edu.sandiego.comp305;
 import java.util.ArrayList;
 
 public class Child extends Person {
+    @Override
+    public String getLifeStage(){
+        return "Child";
+    }
     public ArrayList<Person> getParents(){
         return null;
     }
     public void inheritTraits(Person parent1, person parent2){
     }
+
 }
