af00ee254e04c5d0c6ea50093fe8968fe92e8b12
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 11:16:28 -0700

    Feat: Created constructors for abstract class for subclasses to inherit

--- src/main/java/edu/sandiego/comp305/Person.java
+++ src/main/java/edu/sandiego/comp305/Person.java
@@ -1,6 +1,21 @@
 package edu.sandiego.comp305;
 
-public class Person {
+public abstract class Person {
+    protected String name;
+    protected int age;
+    protected int healthScore;
+    protected String phenotype;
+
+
+    public Person(String name, int age, int healthScore, String phenotype) {
+        this.name = name;
+        this.age = age;
+        this.healthScore = healthScore;
+        this.phenotype = phenotype;
+
+    }
+    }
+
     public String getName(){
         return null;
     }
