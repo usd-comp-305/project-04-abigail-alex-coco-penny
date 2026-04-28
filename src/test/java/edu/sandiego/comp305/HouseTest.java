f77397d19b6b4a6aabc6361401979a938554c1fc
Coco <csequeira@sandiego.edu>
2026-04-28 11:48:49 -0700

    feat: wrote stubs for House.java and HouseTest.java

---
+++ src/test/java/edu/sandiego/comp305/HouseTest.java
@@ -0,0 +1,15 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HouseTest {
+    @Test
+    public void houseAppliesInsurance() {}
+
+    @Test
+    public void houseGetsPremium() {}
+
+    @Test
+    public void houseCalculatesRiskFactorBasedOnLocation() {}
+}
