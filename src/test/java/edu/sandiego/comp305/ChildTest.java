7cf6f374e2464bc290214bd75483c934871e2784
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 13:26:59 -0700

    Feat: initial test stub for child to inherit traits fro parents- should be testable that child gains phenotypes from both parents

--- src/test/java/edu/sandiego/comp305/ChildTest.java
+++ src/test/java/edu/sandiego/comp305/ChildTest.java
@@ -6,8 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ChildTest {
     @Test
-    public void testChild() {
+    public void testGetParentData() {
         Child child = new Child();
-        assertNotNull(child,getParents());
+        assertNotNull(child.getParents());
+    }
+    @Test
+    public void testInheritParentData(){
+        Child child = new Child();
+        Person parent1 = new TestPerson();
+        Person parent2 = new TestPerson();
+        child.inheritTraits(parent1, parent2);
+        assertNotNull(child.getPhenotype());
     }
 }
