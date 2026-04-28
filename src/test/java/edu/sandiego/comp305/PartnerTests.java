850a9fbc4adac1f51e5bc1fefd7265480073d462
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 09:54:35 -0700

    Feat: initial test stub for setCompatibilityScore()

--- src/test/java/edu/sandiego/comp305/PartnerTests.java
+++ src/test/java/edu/sandiego/comp305/PartnerTests.java
@@ -17,10 +17,17 @@ public class PartnerTests {
         partner.setBankBalance(1000);
         assertEquals(1000, partner.getBankBalance());
     }
+
     @Test
     public void testGetInitialCompatibilityScore(){
         Partner partner = new Partner();
         assertEquals(0.0, partner.getCompatibilityScore());
+    }
 
+    @Test
+    public void testSetCompatibilityScore(){
+        Partner partner = new Partner();
+        partner.setCompatibilityScore(0.75);
+        assertEquals(0.75, partner.getCompatibilityScore());
     }
 }
