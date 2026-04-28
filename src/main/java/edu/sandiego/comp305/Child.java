a5df15afd9971b674871c038132f07092dbc148f
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 14:45:29 -0700

    Feat: Include extension from Person class with proper constructor call for public Child implementation

--- src/main/java/edu/sandiego/comp305/Child.java
+++ src/main/java/edu/sandiego/comp305/Child.java
@@ -3,6 +3,11 @@ package edu.sandiego.comp305;
 import java.util.ArrayList;
 
 public class Child extends Person {
+
+    public Child(String name, Age age, int healthScore, String phenotype) {
+        super(name, age, healthScore, phenotype);
+    }
+
     @Override
     public String getLifeStage(){
         return "Child";
