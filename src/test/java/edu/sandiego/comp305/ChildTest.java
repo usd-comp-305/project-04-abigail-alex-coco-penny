06098201e8f5afa8237a7ee64c2cd3a9fad7bd91
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 13:21:08 -0700

    Feat: initial test stub for getParents(0 for child class to be able to get information about parent for child creation

--- src/test/java/edu/sandiego/comp305/ChildTest.java
+++ src/test/java/edu/sandiego/comp305/ChildTest.java
@@ -1,4 +1,13 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class ChildTest {
+    @Test
+    public void testChild() {
+        Child child = new Child();
+        assertNotNull(child,getParents());
+    }
 }
