d2a68692231f9b872b19514628608753bbf0b92d
Penny Yanez <pyanez@sandiego.edu>
2026-04-28 14:48:40 -0700

    Fix: update test class to reflect new constructor implementation in child class

--- src/test/java/edu/sandiego/comp305/ChildTest.java
+++ src/test/java/edu/sandiego/comp305/ChildTest.java
@@ -2,22 +2,23 @@ package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ChildTest {
     @Test
     public void testLifeStageIsChild(){
-        Child child = new Child();
-        assertEquals("Child", child.getLifeStage);
+        Child child = new Child("Child", Age.CHILD, 0, "ABCD");
+        assertEquals("Child", child.getLifeStage());
     }
     @Test
     public void testGetParentData() {
-        Child child = new Child();
+        Child child = new Child("Child", Age.CHILD, 0, "ABCD");
         assertNotNull(child.getParents());
     }
     @Test
     public void testInheritParentData(){
-        Child child = new Child();
+        Child child = new Child("Child", Age.CHILD, 0, "ABCD");
         Person parent1 = new TestPerson();
         Person parent2 = new TestPerson();
         child.inheritTraits(parent1, parent2);
