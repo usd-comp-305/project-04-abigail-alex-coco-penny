92d228ad62afc5c2007242bff3c58de381c864dd
Penny Yanez <pyanez@sandiego.edu>
2026-04-30 09:46:31 -0700

    Initial test stub getDNA()

--- src/test/java/edu/sandiego/comp305/CharacterTest.java
+++ src/test/java/edu/sandiego/comp305/CharacterTest.java
@@ -3,11 +3,17 @@ package edu.sandiego.comp305;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CharacterTest {
     @Test
     public void testGetBankBalance(){
         Character character = new Character();
         assertEquals(1000, character.getBankBalance());
+    }
+    public void testGetDNA(){
+        Character character = new Character();
+        DNA dna = new DNA():
+        assertNotNull(character.getDNA(dna));
     }
 }
