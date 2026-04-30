2168b8b17bbdd0063837340824f78c2bfaaf7882
Penny Yanez <pyanez@sandiego.edu>
2026-04-30 10:08:37 -0700

    Update character test to include constructor param

--- src/test/java/edu/sandiego/comp305/CharacterTest.java
+++ src/test/java/edu/sandiego/comp305/CharacterTest.java
@@ -8,18 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class CharacterTest {
     @Test
     public void testGetLifeStage(){
-        Character character = new Character();
-        assertEquals("Character", character.getLifeStage())
+        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
+        assertEquals("Character", character.getLifeStage());
     }
     @Test
     public void testGetBankBalance(){
-        Character character = new Character();
+        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
         assertEquals(1000, character.getBankBalance());
     }
 
     @Test
     public void testGetDNA(){
-        Character character = new Character();
+        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
         DNA dna = new DNA():
         assertNotNull(character.getDNA());
     }
