deb83e8b38baab7c90893ae3cc6546ab23ba84e0
Penny Yanez <pyanez@sandiego.edu>
2026-04-30 10:00:03 -0700

    Initial method stub getLifeStage()

--- src/main/java/edu/sandiego/comp305/Character.java
+++ src/main/java/edu/sandiego/comp305/Character.java
@@ -1,11 +1,16 @@
 package edu.sandiego.comp305;
 
-public class Character {
+public class Character extends Person{
     public double getBankBalance(){
         return 0.0;
     }
 
     public DNA getDNA(){
         return null;
+    }
+
+    @Override
+    public String getLifeStage(){
+        return "Character";
     }
 }
