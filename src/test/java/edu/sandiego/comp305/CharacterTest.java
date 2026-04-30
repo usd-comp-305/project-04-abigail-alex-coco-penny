7faf5c91f42cf290825917e50d53d4a95e23d50e
Penny Yanez <pyanez@sandiego.edu>
2026-04-30 09:57:55 -0700

    Initial test stub getLifeStage abstract method()

--- src/test/java/edu/sandiego/comp305/CharacterTest.java
+++ src/test/java/edu/sandiego/comp305/CharacterTest.java
@@ -7,13 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CharacterTest {
     @Test
+    public void testGetLifeStage(){
+        Character character = new Character();
+        assertEquals("Character", character.getLifeStage())
+    }
+    @Test
     public void testGetBankBalance(){
         Character character = new Character();
         assertEquals(1000, character.getBankBalance());
     }
+
+    @Test
     public void testGetDNA(){
         Character character = new Character();
         DNA dna = new DNA():
-        assertNotNull(character.getDNA(dna));
+        assertNotNull(character.getDNA());
     }
+
+
+
+
 }
