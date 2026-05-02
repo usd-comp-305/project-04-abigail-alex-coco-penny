0cd722b52946eb790259f8c2afb626f5092df48e
Coco <csequeira@sandiego.edu>
2026-05-02 15:05:56 -0700

    refactor: combined car class and car enum into an enum

--- src/main/java/edu/sandiego/comp305/Car.java
+++ src/main/java/edu/sandiego/comp305/Car.java
@@ -1,11 +1,22 @@
 package edu.sandiego.comp305;
 
-public class Car implements Insurable {
-    private final int value;
-    private final String type;
-    private final int year;
+public enum Car implements Insurable {
+    MICRO (4995, new String[]{"Smart", "Fortwo"}, 2015),
+    HATCHBACK (6750, new String[]{"Honda", "Fit"}, 2008),
+    SEDAN (18585, new String[]{"Nissan", "Versa"}, 2024),
+    SUV (16749, new String[]{"Subaru", "Outback"},2018 ),
+    SPORT (36998, new String[]{"Jaguar", "F-Type P300"}, 2020),
+    PICK_UP (24495, new String[]{"Chevorlet", "Silverado"}, 2021),
+    LIMO (110000, new String[]{"Rolls Royce", "Ghost Exotic"}, 2004),
+    VAN (61545, new String[]{"Volkswagen", "Microbus"}, 2025),
+    CONVERTIBLE (7888, new String[]{"BMW", "328i"}, 2010),
+    WAGON (12998, new String[]{"Volkswagen", "Golf"}, 2016);
 
-    public Car (int value, String type, int year) {
+    public final int value;
+    public final String[] type;
+    public final int year;
+
+    Car(int value, String[] type, int year) {
         this.value = value;
         this.type = type;
         this.year = year;
@@ -24,7 +35,7 @@ public class Car implements Insurable {
         return this.value;
     }
 
-    public String getType() {
+    public String[] getType() {
         return this.type;
     }
 
