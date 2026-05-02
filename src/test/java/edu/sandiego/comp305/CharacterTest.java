6565dbc84c0b9ca59882f786defd7a5a0e716167
Penny Yanez <pyanez@sandiego.edu>
2026-05-02 13:02:11 -0700

    Feat: include before each for character with constrcutor to fix redundancies

--- src/test/java/edu/sandiego/comp305/CharacterTest.java
+++ src/test/java/edu/sandiego/comp305/CharacterTest.java
@@ -1,32 +1,37 @@
 package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CharacterTest {
+
+    private Character character;
+
+    @BeforeEach
+    public void setUp() {
+        final DNA dna = new DNA(new HashMap<>());
+        character = new Character ("Name", Age.YOUNG_ADULT, 100, dna);
+    }
     @Test
     public void testGetLifeStage(){
-        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
         assertEquals("Character", character.getLifeStage());
     }
     @Test
     public void testGetBankBalance(){
-        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
         assertEquals(1000, character.getBankBalance());
     }
 
     @Test
     public void testGetDNA(){
-        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
-        DNA dna = new DNA():
         assertNotNull(character.getDNA());
     }
 
     @Test
     public void testChooseLocation(){
-        Character character = new Character("Name", Age.YOUNG_ADULT, 0, "ABCD");
         Location location = new Location("San Diego", "CA");
         character.chooseLocation(location);
         assertEquals("San Diego", location.getCity());
