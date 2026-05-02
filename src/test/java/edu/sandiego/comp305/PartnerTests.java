a82bdd0e6f2c1c93c077471a7d64008651152deb
Penny Yanez <pyanez@sandiego.edu>
2026-05-02 14:52:33 -0700

    Feat: Before each set up of partner

--- src/test/java/edu/sandiego/comp305/PartnerTests.java
+++ src/test/java/edu/sandiego/comp305/PartnerTests.java
@@ -1,37 +1,43 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PartnerTests {
+
+    private Partner partner;
+
+    @BeforeEach
+    public void setUp() {
+        final DNA dna = new DNA(new HashMap<>());
+        partner = new Partner("Name", Age.MIDDLE_AGE, 80, dna);
+    }
     @Test
     public void testLifeStageIsPartner(){
-        Partner partner = new Partner("Partner", Age.MIDDLE_AGE, 0, "ABCD");
         assertEquals("Partner", partner.getLifeStage());
     }
     @Test
     public void testGetBankBalance(){
-        Partner partner = new Partner("Partner", Age.MIDDLE_AGE, 0, "ABCD");
         assertEquals(1000, partner.getBankBalance());
     }
 
     @Test
     public void testSettBankBalance(){
-        Partner partner = new Partner("Partner", Age.MIDDLE_AGE, 0, "ABCD");
         partner.setBankBalance(1000);
         assertEquals(1000, partner.getBankBalance());
     }
 
     @Test
     public void testGetInitialCompatibilityScore(){
-        Partner partner = new Partner("Partner", Age.MIDDLE_AGE, 0, "ABCD");
         assertEquals(0.0, partner.getCompatibilityScore());
     }
 
     @Test
     public void testSetCompatibilityScore(){
-        Partner partner = new Partner("Partner", Age.MIDDLE_AGE, 0, "ABCD");
         partner.setCompatibilityScore(0.75);
         assertEquals(0.75, partner.getCompatibilityScore());
     }
