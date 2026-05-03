449e63fa1551165a3053f055e0eca32c71c3bd5e
Abigail Callahan <abigailcallahan@sandiego.edu>
2026-05-03 15:43:33 -0700

    feat: added phenotypes to Allele enum

--- src/main/java/edu/sandiego/comp305/Allele.java
+++ src/main/java/edu/sandiego/comp305/Allele.java
@@ -1,8 +1,24 @@
 package edu.sandiego.comp305;
 
 public enum Allele {
-    EYE_COLOR,
-    HEIGHT,
-    DISEASE_RISK,
-    HAIR_COLOR
+    EYE_COLOR ("brown", "blue"),
+    HEIGHT ("tall", "short"),
+    DISEASE_RISK ("high", "low"),
+    HAIR_COLOR ("brown", "blonde");
+
+    private final String dominantTrait;
+    private final String recessiveTrait;
+
+    Allele(String dominantTrait, String recessiveTrait) {
+        this.dominantTrait = dominantTrait;
+        this.recessiveTrait = recessiveTrait;
+    }
+
+    public String getDominantTrait() {
+        return dominantTrait;
+    }
+
+    public String getRecessiveTrait() {
+        return recessiveTrait;
+    }
 }
