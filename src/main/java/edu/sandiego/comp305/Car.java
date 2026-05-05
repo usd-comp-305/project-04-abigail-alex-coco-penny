9bb35a34e161c46681fdefdf794c56c68a236f27
Coco <csequeira@sandiego.edu>
2026-05-05 11:25:42 -0700

    feat: added baseRate to Car enum

--- src/main/java/edu/sandiego/comp305/Car.java
+++ src/main/java/edu/sandiego/comp305/Car.java
@@ -1,25 +1,27 @@
 package edu.sandiego.comp305;
 
 public enum Car implements Insurable {
-    MICRO (4995, new String[]{"Smart", "Fortwo"}, 2015),
-    HATCHBACK (6750, new String[]{"Honda", "Fit"}, 2008),
-    SEDAN (18585, new String[]{"Nissan", "Versa"}, 2024),
-    SUV (16749, new String[]{"Subaru", "Outback"},2018 ),
-    SPORT (36998, new String[]{"Jaguar", "F-Type P300"}, 2020),
-    PICK_UP (24495, new String[]{"Chevorlet", "Silverado"}, 2021),
-    LIMO (110000, new String[]{"Rolls Royce", "Ghost Exotic"}, 2004),
-    VAN (61545, new String[]{"Volkswagen", "Microbus"}, 2025),
-    CONVERTIBLE (7888, new String[]{"BMW", "328i"}, 2010),
-    WAGON (12998, new String[]{"Volkswagen", "Golf"}, 2016);
+    MICRO (4995, new String[]{"Smart", "Fortwo"}, 2015, 24),
+    HATCHBACK (6750, new String[]{"Honda", "Fit"}, 2008, 26),
+    SEDAN (18585, new String[]{"Nissan", "Versa"}, 2024, 28),
+    SUV (16749, new String[]{"Subaru", "Outback"},2018, 31),
+    SPORT (36998, new String[]{"Jaguar", "F-Type P300"}, 2020, 45),
+    PICK_UP (24495, new String[]{"Chevorlet", "Silverado"}, 2021, 32),
+    LIMO (110000, new String[]{"Rolls Royce", "Ghost Exotic"}, 2004, 50),
+    VAN (61545, new String[]{"Volkswagen", "Microbus"}, 2025, 29),
+    CONVERTIBLE (7888, new String[]{"BMW", "328i"}, 2010, 36),
+    WAGON (12998, new String[]{"Volkswagen", "Golf"}, 2016, 27);
 
     public final int value;
     public final String[] type;
     public final int year;
+    public final int baseRate;
 
-    Car(int value, String[] type, int year) {
+    Car(int value, String[] type, int year, int baseRate) {
         this.value = value;
         this.type = type;
         this.year = year;
+        this.baseRate = baseRate;
     }
 
     @Override
@@ -28,6 +30,7 @@ public enum Car implements Insurable {
 
     @Override
     public double getPremium() {
+
         return 0.0;
     }
 
