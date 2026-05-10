8ef111fda0e27dcef507488d71b9b6e3fa36e5a7
Abigail Callahan <abigailcallahan@sandiego.edu>
2026-05-10 13:14:10 -0700

    test: set up helper method for tests

--- src/test/java/edu/sandiego/comp305/DNATests.java
+++ src/test/java/edu/sandiego/comp305/DNATests.java
@@ -1,5 +1,25 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DNATests {
+
+    public AllelePair mockBB;
+    public AllelePair mockBb;
+    public AllelePair mockbB;
+    public AllelePair mockbb;
+
+    // helper method for the tests
+    public DNA createDNAMock(AllelePair mockPair) {
+        Map<Allele, AllelePair> traits = new HashMap<>();
+        for(Allele allele: Allele.values()) {
+            traits.put(allele, mockPair);
+        }
+        return new DNA(traits);
+    }
 
 }
