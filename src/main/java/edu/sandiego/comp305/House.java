f6d62cda7413451a4728b74d44c855287cf23dd2
Coco <csequeira@sandiego.edu>
2026-05-10 11:43:40 -0700

    chore: changed value and mortgage balances in house types to be more accurate

--- src/main/java/edu/sandiego/comp305/House.java
+++ src/main/java/edu/sandiego/comp305/House.java
@@ -1,17 +1,24 @@
 package edu.sandiego.comp305;
 
-public class House implements Insurable {
+public enum House implements Insurable {
+    APARTMENT (250000, 180000),
+    SUBURBAN_HOME (450000, 320000),
+    CONDO (350000, 240000),
+    MANSION (2500000, 1800000),
+    CASTLE (5000000, 3500000),
+    COTTAGE (175000, 120000),
+    MOBILE_HOME (95000, 65000),
+    TREE_HOUSE (40000, 15000),
+    HUT (25000, 10000),
+    HOUSE_BOAT (220000, 150000);
+
     private final int value;
 
     private final int mortgageBalance;
 
-    private final int address;
-
-    public House (final int value, final int mortgageBalance,
-                  final int address) {
+    House (final int value, final int mortgageBalance) {
         this.value = value;
         this.mortgageBalance = mortgageBalance;
-        this.address = address;
     }
 
     public int getValue() {
@@ -20,10 +27,6 @@ public class House implements Insurable {
 
     public int getMortgageBalance() {
         return this.mortgageBalance;
-    }
-
-    public int getAddress() {
-        return this.address;
     }
 
     @Override
