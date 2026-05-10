7b04aa7ce7b57216e3a828e86e9765c11cce637a
Coco <csequeira@sandiego.edu>
2026-05-10 11:33:03 -0700

    feat: updated riskFactors for locations

--- src/main/java/edu/sandiego/comp305/Location.java
+++ src/main/java/edu/sandiego/comp305/Location.java
@@ -1,16 +1,16 @@
 package edu.sandiego.comp305;
 
 public enum Location {
-    NYC ("New York", "NY", 0.2),
-    LA ("Los Angeles", "CA",0.3),
-    CHICAGO ("Chicago", "IL", 0.2),
-    HOUSTON ("Houston", "TX", 0.4),
-    PHOENIX ("Phoenix", "AZ", 0.5),
-    SAN_DIEGO ("San Diego", "CAa" , 0.6),
-    SEATTLE ("Seattle", "WA", 0.1),
-    DENVER ("Denver", "CO", 0.5),
-    BOSTON ("Boston", "MA", 0.6),
-    MIAMI ("Miami", "FL", 0.8);
+    MIAMI ("Miami", "FL", 0.9),
+    NEW_YORK ("New York", "NY", 0.8),
+    PHILADELPHIA ("Philadelphia", "PA",0.7),
+    CHICAGO ("Chicago", "IL", 0.7),
+    HOUSTON ("Houston", "TX", 0.5),
+    BOSTON ("Boston", "MA", 0.4),
+    PHOENIX ("Phoenix", "AZ", 0.4),
+    SAN_DIEGO ("San Diego", "CAa" , 0.3),
+    DENVER ("Denver", "CO", 0.3),
+    SEATTLE ("Seattle", "WA", 0.2);
 
     private final String city;
 
