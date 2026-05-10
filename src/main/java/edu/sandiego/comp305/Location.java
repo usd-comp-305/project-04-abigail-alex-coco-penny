3c3fa6e1f168800ca0e3ab058d33319f7838c874
Coco <csequeira@sandiego.edu>
2026-05-10 11:27:00 -0700

    refactor: changed format of Location enum and updated the call it in PartnerTest and CharacterTest

--- src/main/java/edu/sandiego/comp305/Location.java
+++ src/main/java/edu/sandiego/comp305/Location.java
@@ -1,13 +1,27 @@
 package edu.sandiego.comp305;
 
-public class Location {
+public enum Location {
+    NYC ("New York", "NY", 0.2),
+    LA ("Los Angeles", "CA",0.3),
+    CHICAGO ("Chicago", "IL", 0.2),
+    HOUSTON ("Houston", "TX", 0.4),
+    PHOENIX ("Phoenix", "AZ", 0.5),
+    SAN_DIEGO ("San Diego", "CAa" , 0.6),
+    SEATTLE ("Seattle", "WA", 0.1),
+    DENVER ("Denver", "CO", 0.5),
+    BOSTON ("Boston", "MA", 0.6),
+    MIAMI ("Miami", "FL", 0.8);
+
     private String city;
 
     private String state;
 
-    public Location (final String city, final String state) {
+    private double riskFactor;
+
+    Location (final String city, final String state, final double riskFactor) {
         this.city = city;
         this.state = state;
+        this.riskFactor = riskFactor;
     }
 
     public String getCity () {
@@ -17,4 +31,7 @@ public class Location {
     public String getState() {
         return this.state;
     }
+
+    public double getRiskFactor() { return this.riskFactor; }
+
 }
