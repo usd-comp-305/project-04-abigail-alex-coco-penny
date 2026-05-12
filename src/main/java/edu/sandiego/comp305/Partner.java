cc7e1ff30f3f392ead5d88c33b00cbeb0e1eabf0
Penny Yanez <pyanez@sandiego.edu>
2026-05-11 22:00:51 -0700

    FIx: remove duplicative bankBalance()

--- src/main/java/edu/sandiego/comp305/Partner.java
+++ src/main/java/edu/sandiego/comp305/Partner.java
@@ -2,8 +2,6 @@ package edu.sandiego.comp305;
 
 public class Partner extends Person{
 
-    private double bankBalance;
-
     private double compatibilityScore;
 
     private Location location;
@@ -15,9 +13,6 @@ public class Partner extends Person{
         this.compatibilityScore = 0.0;
     }
 
-    public double getBankBalance(){
-        return bankBalance;
-    }
 
     public double getCompatibilityScore(){
         return compatibilityScore;
@@ -34,10 +29,6 @@ public class Partner extends Person{
 
     public void setCompatibilityScore(final double compatibilityScore){
         this.compatibilityScore = compatibilityScore;
-    }
-
-    public void setBankBalance(final double bankBalance){
-        this.bankBalance = bankBalance;
     }
 
     public void chooseCareer(final Career career){
