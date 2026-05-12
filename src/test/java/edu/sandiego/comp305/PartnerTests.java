a8ab4b396e5dc0f7edb4c03dca8ac7a97e0da276
Penny Yanez <pyanez@sandiego.edu>
2026-05-11 22:15:37 -0700

    Test: update test to reflect life stage update

--- src/test/java/edu/sandiego/comp305/PartnerTests.java
+++ src/test/java/edu/sandiego/comp305/PartnerTests.java
@@ -19,7 +19,7 @@ public class PartnerTests {
 
     @Test
     public void testLifeStageIsPartner(){
-        assertEquals("Partner", partner.getLifeStage());
+        assertEquals("Life with a Partner", partner.getLifeStage());
     }
 
     @Test
