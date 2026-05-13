11def5b31f2d0625c205fd908b04fb99ee073985
Penny Yanez <pyanez@sandiego.edu>
2026-05-13 10:08:36 -0700

    Fix: remove extrenuous method call in child class-- already implemented in person

--- src/main/java/edu/sandiego/comp305/Child.java
+++ src/main/java/edu/sandiego/comp305/Child.java
@@ -39,11 +39,4 @@ public class Child extends Person {
         this.wentToCollege = wentToCollege;
     }
 
-    public Phenotype getPhenotype(){
-        return new Phenotype(
-                getDna().calculatePhenotypeFromAllele(Allele.EYE_COLOR),
-                getDna().calculatePhenotypeFromAllele(Allele.HEIGHT),
-                getDna().calculatePhenotypeFromAllele(Allele.HAIR_COLOR));
-    }
-
 }
